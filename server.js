e516cdb7745d430c12f97b2a5c283cd09ef6f7e5
raciopporyan <racioppo.ryan@gmail.com>
2018-08-16 20:16:46 -0400

    not sure whats different tbh, i want that green square

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+
 var logger = require('morgan');
 
 var request = require('request');
@@ -11,4 +14,23 @@ var port = 4545;
 
 app.listen(process.env.PORT || port, function(){
     console.log('server running on port 4545');
+});
+
+var databaseUrl = 'scraper';
+var collections = ["scrapedData"];
+
+app.get("/", function(req, res){
+    res.send("You are on the index");
+});
+
+
+app.get("/scrape", function(req, res){
+    request("https://www.imdb.com/chart/top", function(err, response, html){
+        var $ = cheerio.load(html);
+
+        
+        console.log($);
+
+
+    });
 });
